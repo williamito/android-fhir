a94ea67a50c2d884e2e65bd1c60610d9616d9d7f
DeeproChoudhury <72229274+DeeproChoudhury@users.noreply.github.com>
2021-08-19 00:51:31 +0100

    Able to parse proto file and output json result, completing pipeline

--- Experimentalprotos/src/main/java/Main.java
+++ Experimentalprotos/src/main/java/Main.java
@@ -1,11 +1,17 @@
 import com.google.fhir.common.InvalidFhirException;
+import com.google.fhir.r4.core.Patient;
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
 
   public static void main(String[] args) throws IOException, InvalidFhirException {
-    new JsonFormatGenerate()
-        .generateProtoTxt(new String[] {"label"}, null);
+//    new JsonFormatGenerate()
+//        .generateProtoTxt(new String[] {"label"}, null);
+
+    File filename = new File("/android-fhir/filename.prototxt").getAbsoluteFile();
+
+    new JsonFormatBase().parseToJson(filename, Patient.newBuilder());
   }
 
 }

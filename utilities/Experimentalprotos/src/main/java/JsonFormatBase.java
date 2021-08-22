278c24b505f19e9167242997e5060fc6627f314b
DeeproChoudhury <72229274+DeeproChoudhury@users.noreply.github.com>
2021-08-22 04:06:21 +0100

    Abstract and streamline file methods

--- utilities/Experimentalprotos/src/main/java/JsonFormatBase.java
+++ utilities/Experimentalprotos/src/main/java/JsonFormatBase.java
@@ -2,21 +2,13 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.fhir.r4.core.Patient;
 import com.google.fhir.shaded.common.io.Files;
-import com.google.fhir.shaded.protobuf.CodedInputStream;
-import com.google.fhir.shaded.protobuf.GeneratedMessageV3;
 import com.google.fhir.shaded.protobuf.Message;
-import com.google.fhir.shaded.protobuf.MessageOrBuilder;
-import com.google.fhir.shaded.protobuf.TextFormat.ParseException;
-import com.google.gson.JsonParser;
-import com.google.fhir.shaded.protobuf.Message.Builder;
 import com.google.fhir.common.JsonFormat;
 import com.google.fhir.shaded.protobuf.TextFormat;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.time.ZoneId;
 
 
@@ -28,16 +20,60 @@ public class JsonFormatBase {
   protected TextFormat.Parser textParser = TextFormat.getParser();
 
   protected JsonFormat.Printer jsonPrinter = JsonFormat.getPrinter();
+  
+  private final String examplesDir = "/android-fhir/utilities/examplefiles";
+  
+  public enum FileType {
+      JSON,
+      PROTOTXT,
+      PROTOBINARY
+  }
 
   protected String loadJson(String filename) throws IOException {
     File file = new File("D:/android-fhir/" + filename);
     return Files.asCharSource(file, UTF_8).read();
   }
+  
+  public String getProtoBinaryPath(String filename) {
+    String pathName = examplesDir + "/protobinary/" + filename + ".proto";
+    return pathName;
+  }
+  
+  public String getProtoTxtPath(String filename) {
+    String pathName = examplesDir + "/prototxt/" + filename + ".prototxt";
+    return pathName;
+  }
+  
+  public String getJsonPath(String filename) {
+    String pathName = examplesDir + "/json/" + filename + ".json";
+    return pathName;
+  }
+  
+  public File getExampleFile(String filename, FileType fileType)  {
+    String filePath;
+    
+    switch (fileType) {
+      case JSON:
+        filePath = getJsonPath(filename);
+        break;
+      case PROTOTXT:
+        filePath = getProtoTxtPath(filename);
+        break;
+      case PROTOBINARY:
+        filePath = getProtoBinaryPath(filename);
+        break;
+      default:
+        throw new IllegalArgumentException("Invalid file type");
+    }
+    
+    File resultFile = new File(filePath);
+    return resultFile;
+  }
 
   protected void parseToProto(String name, com.google.fhir.shaded.protobuf.Message.Builder builder)
       throws IOException {
 
-    String realFileName = "/android-fhir/" + name + ".json";
+    String realFileName = getJsonPath(name);
 
     jsonParser.merge(loadJson(realFileName), builder);
 
@@ -49,21 +85,24 @@ public class JsonFormatBase {
     return builder.toString();
   }
 
-  protected void createProtoBinaryFile(String filename, Message.Builder builder)
+  protected File createProtoBinaryFile(String filename, Message.Builder builder)
       throws IOException {
-    File file = new File("/android-fhir/" + filename + ".prototxt");
+    
+    File file = getExampleFile(filename, FileType.PROTOTXT);
 
     textParser.merge(Files.asCharSource(file, UTF_8).read(), builder);
 
-    File newFile = new File("/android-fhir/" + filename + ".proto");
+    File protoFile = getExampleFile(filename, FileType.PROTOBINARY);
 
     builder.build()
-        .writeTo(new FileOutputStream(newFile));
+        .writeTo(new FileOutputStream(protoFile));
 
     Message newMessage = builder.getDefaultInstanceForType().getParserForType()
-        .parseFrom(new FileInputStream(newFile));
+        .parseFrom(new FileInputStream(protoFile));
 
     System.out.println(newMessage);
+    
+    return protoFile;
   }
 
   protected String parseToJson(File file, Message.Builder builder) throws IOException {
--- utilities/Experimentalprotos/src/test/java/ProtoBinaryFHIRPathTests.java
+++ utilities/Experimentalprotos/src/test/java/ProtoBinaryFHIRPathTests.java
@@ -1,3 +1,4 @@
+import com.google.fhir.r4.core.CompositionAttestationModeCode.Builder;
 import com.google.fhir.r4.core.Patient;
 import com.google.fhir.r4.core.Practitioner;
 import java.io.File;
@@ -9,6 +10,70 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ProtoBinaryFHIRPathTests {
+  
+  String jsonPractitioner = "{\n"
+      + "  \"resourceType\": \"Practitioner\",\n"
+      + "  \"id\": \"example\",\n"
+      + "  \"text\": {\n"
+      + "    \"status\": \"generated\",\n"
+      + "    \"div\": \"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\">\\n      <p>Dr Adam Careful is a Referring Practitioner for Acme Hospital from 1-Jan 2012 to 31-Mar\\n        2012</p>\\n    </div>\"\n"
+      + "  },\n"
+      + "  \"identifier\": [\n"
+      + "    {\n"
+      + "      \"system\": \"http://www.acme.org/practitioners\",\n"
+      + "      \"value\": \"23\"\n"
+      + "    }\n"
+      + "  ],\n"
+      + "  \"active\": true,\n"
+      + "  \"name\": [\n"
+      + "    {\n"
+      + "      \"family\": \"Careful\",\n"
+      + "      \"given\": [\n"
+      + "        \"Adam\"\n"
+      + "      ],\n"
+      + "      \"prefix\": [\n"
+      + "        \"Dr\"\n"
+      + "      ]\n"
+      + "    }\n"
+      + "  ],\n"
+      + "  \"address\": [\n"
+      + "    {\n"
+      + "      \"use\": \"home\",\n"
+      + "      \"line\": [\n"
+      + "        \"534 Erewhon St\"\n"
+      + "      ],\n"
+      + "      \"city\": \"PleasantVille\",\n"
+      + "      \"state\": \"Vic\",\n"
+      + "      \"postalCode\": \"3999\"\n"
+      + "    }\n"
+      + "  ],\n"
+      + "  \"qualification\": [\n"
+      + "    {\n"
+      + "      \"identifier\": [\n"
+      + "        {\n"
+      + "          \"system\": \"http://example.org/UniversityIdentifier\",\n"
+      + "          \"value\": \"12345\"\n"
+      + "        }\n"
+      + "      ],\n"
+      + "      \"code\": {\n"
+      + "        \"coding\": [\n"
+      + "          {\n"
+      + "            \"system\": \"http://terminology.hl7.org/CodeSystem/v2-0360/2.7\",\n"
+      + "            \"code\": \"BS\",\n"
+      + "            \"display\": \"Bachelor of Science\"\n"
+      + "          }\n"
+      + "        ],\n"
+      + "        \"text\": \"Bachelor of Science\"\n"
+      + "      },\n"
+      + "      \"period\": {\n"
+      + "        \"start\": \"1995\"\n"
+      + "      },\n"
+      + "      \"issuer\": {\n"
+      + "        \"display\": \"Example University\"\n"
+      + "      }\n"
+      + "    }\n"
+      + "  ]\n"
+      + "}";
 
 //  @BeforeAll
 //  static void createPatientProtoBinary() throws IOException {
@@ -48,75 +113,22 @@ public class ProtoBinaryFHIRPathTests {
   @Test
   public void TestPractitionerTxt() throws IOException {
 
-    String string = new JsonFormatBase().parseStringToProto("{\n"
-        + "  \"resourceType\": \"Practitioner\",\n"
-        + "  \"id\": \"example\",\n"
-        + "  \"text\": {\n"
-        + "    \"status\": \"generated\",\n"
-        + "    \"div\": \"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\">\\n      <p>Dr Adam Careful is a Referring Practitioner for Acme Hospital from 1-Jan 2012 to 31-Mar\\n        2012</p>\\n    </div>\"\n"
-        + "  },\n"
-        + "  \"identifier\": [\n"
-        + "    {\n"
-        + "      \"system\": \"http://www.acme.org/practitioners\",\n"
-        + "      \"value\": \"23\"\n"
-        + "    }\n"
-        + "  ],\n"
-        + "  \"active\": true,\n"
-        + "  \"name\": [\n"
-        + "    {\n"
-        + "      \"family\": \"Careful\",\n"
-        + "      \"given\": [\n"
-        + "        \"Adam\"\n"
-        + "      ],\n"
-        + "      \"prefix\": [\n"
-        + "        \"Dr\"\n"
-        + "      ]\n"
-        + "    }\n"
-        + "  ],\n"
-        + "  \"address\": [\n"
-        + "    {\n"
-        + "      \"use\": \"home\",\n"
-        + "      \"line\": [\n"
-        + "        \"534 Erewhon St\"\n"
-        + "      ],\n"
-        + "      \"city\": \"PleasantVille\",\n"
-        + "      \"state\": \"Vic\",\n"
-        + "      \"postalCode\": \"3999\"\n"
-        + "    }\n"
-        + "  ],\n"
-        + "  \"qualification\": [\n"
-        + "    {\n"
-        + "      \"identifier\": [\n"
-        + "        {\n"
-        + "          \"system\": \"http://example.org/UniversityIdentifier\",\n"
-        + "          \"value\": \"12345\"\n"
-        + "        }\n"
-        + "      ],\n"
-        + "      \"code\": {\n"
-        + "        \"coding\": [\n"
-        + "          {\n"
-        + "            \"system\": \"http://terminology.hl7.org/CodeSystem/v2-0360/2.7\",\n"
-        + "            \"code\": \"BS\",\n"
-        + "            \"display\": \"Bachelor of Science\"\n"
-        + "          }\n"
-        + "        ],\n"
-        + "        \"text\": \"Bachelor of Science\"\n"
-        + "      },\n"
-        + "      \"period\": {\n"
-        + "        \"start\": \"1995\"\n"
-        + "      },\n"
-        + "      \"issuer\": {\n"
-        + "        \"display\": \"Example University\"\n"
-        + "      }\n"
-        + "    }\n"
-        + "  ]\n"
-        + "}", Practitioner.newBuilder());
+    String string = new JsonFormatBase().
+        parseStringToProto(jsonPractitioner, Practitioner.newBuilder());
 
     System.out.println(string);
 
     String practitioner = new JsonFormatBase().parseToJson(string, Practitioner.newBuilder());
 
-    System.out.println(new ProtoFHIRPathFiles().processJSON(practitioner, "qualification.issuer"));
+    System.out.println(new ProtoFHIRPathFiles().processJSON(practitioner, 
+        "qualification.issuer"));
+  }
+
+  @Test
+  public void TestConvertToProto() throws IOException {
+
+    Patient.Builder patientBuilder = Patient.newBuilder();
+    new JsonFormatBase().createProtoBinaryFile("filename", patientBuilder);
   }
 
 }
